87d685ab7bca68d61754416ed315af2f33b0f643
Zonick00 <Zonick00@users.noreply.github.com>
2019-07-26 12:32:42 -0500

    Update imagen.cargar.php

--- ws_CB/ws/imagen.cargar.php
+++ ws_CB/ws/imagen.cargar.php
@@ -5,7 +5,7 @@ require_once '../util/funciones/Funciones.clase.php';
 
 
 $p_nombreImagen = $_FILES["imagen"]["name"];
-$carpeta_destino ="../imagenes/";
+$carpeta_destino ="ws-CB/imagenes/";
         
 move_uploaded_file($_FILES["imagen"]["tmp_name"], $carpeta_destino.$p_nombreImagen);
 
