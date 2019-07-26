cb7315d90a55a505ccf3c0f9aefd6a421c6ece89
Zonick00 <Zonick00@users.noreply.github.com>
2019-07-26 14:27:41 -0500

    Update imagen.cargar.php

--- ws_CB/ws/imagen.cargar.php
+++ ws_CB/ws/imagen.cargar.php
@@ -5,8 +5,8 @@ require_once '../util/funciones/Funciones.clase.php';
 
 
 $p_nombreImagen = $_FILES["imagen"]["name"];
-$carpeta_destino ="ws-CB/imagenes/";
+$carpeta_destino ="ws-CB/imagenes/".$p_nombreImagen;
         
-move_uploaded_file($_FILES["imagen"]["tmp_name"], $carpeta_destino.$p_nombreImagen);
+move_uploaded_file($_FILES["imagen"]["tmp_name"], $carpeta_destino);
 
 Funciones::imprimeJSON($carpeta_destino);
