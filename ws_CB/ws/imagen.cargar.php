3f9ebd1b1734b162d2acf793c48407da1cdedf79
Zonick00 <Zonick00@users.noreply.github.com>
2019-07-26 12:00:09 -0500

    Update imagen.cargar.php

--- ws_CB/ws/imagen.cargar.php
+++ ws_CB/ws/imagen.cargar.php
@@ -3,20 +3,12 @@
 require_once '../logica/Sesion.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
 
-if (! isset($_REQUEST["token"])){
-    Funciones::imprimeJSON("Debe especificar un token");
-    exit();
-}
 
-try {
-    if (validarToken($_REQUEST["token"])) {
-        Funciones::imprimeJSON("hola");
-        /*$p_nombreImagen = $_FILES["imagen"]["name"];
-        $carpeta_destino = '../imagenes/';
+$p_nombreImagen = $_FILES["imagen"]["name"];
+$carpeta_destino = '../imagenes/';
         
-        move_uploaded_file($_FILES["imagen"]["tmp_name"], $carpeta_destino.$p_nombreImagen);
-        */
-    }
+move_uploaded_file($_FILES["imagen"]["tmp_name"], $carpeta_destino.$p_nombreImagen);
+
     
 } catch (Exception $exc) {
     Funciones::imprimeJSON($exc->getMessage());
